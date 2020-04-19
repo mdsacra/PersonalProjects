43cbcf576e8fa446df12e037d910d8de87abec2d
mdsacra <mdsacra@gmail.com>
2020-04-19 12:37:21 -0300

    Home 99% done. But without database.
    Some details are still missing, but that was what I planned.

--- Web/Home/homejs.js
+++ Web/Home/homejs.js
@@ -1,51 +1,88 @@
+var main = document.getElementById('principal')
+var box = 0
 
-
-function criaformulario(a) {
-    a.style.margin = '2%'
-    a.style.background = '#635a5a63'
-    a.style.width = '100%'
-    
-    
+function addsocial() {
+    formulario()
+    box = 1
 }
 
-function adicionar() {
-    
-    var main = document.getElementById('principal')
+function addwork() {
+    formulario()
+    box = 2
+}
 
+function addenter() {
+    formulario()
+    box = 3
+}
+
+
+/* OBJETO FORMULARIO */
+function formulario() {
+    
     var container = document.createElement('div')
     container.setAttribute('class', 'container')
+    container.setAttribute('id', 'boxform')
     main.appendChild(container)
 
     var form = document.createElement('div')
-    form.setAttribute('class', 'form')
+    form.setAttribute('id', 'form')
     container.appendChild(form)
-    
 
-    form.innerHTML = 'Adicione seu Link: '
-    criaformulario(form)
+    form.innerHTML = `Adicione um Link: </br>`
     
     var input = document.createElement(`input`)
-    input.setAttribute(`class`, `pagina`)
-    input.setAttribute(`id`, `link`)
+    input.setAttribute(`id`, `insLink`)
     input.setAttribute(`type`, `url`)
     form.appendChild(input)
 
-    var img = document.createElement(`input`)
+    var button = document.createElement('input')
+    button.setAttribute('id', 'addFav')
+    button.setAttribute('onclick', 'savelink()')
+    button.type = 'button'
+    button.value = 'Salvar'
+    form.appendChild(button)
+    
+    /*var img = document.createElement(`input`)
     img.setAttribute(`class`, `pagina`)
     input.setAttribute(`type`, `src`)
-    form.appendChild(img)
-    
-    var button = document.createElement('button')
-    button.setAttribute('class', 'addFav')
-    button.setAttribute('onclick', 'savelink()')
-    button.style.width = '80px'
-    button.style.height = '30px'
-    form.appendChild(button)
+    form.appendChild(img)*/
+
 }
 
+
 function savelink() {
-    var linktxt = document.getElementById('link')
+    var linktxt = document.getElementById('insLink')
     var link = linktxt.value
-    var lista = document.getElementById('teste')
-    lista.innerHTML += `</br>${link}`
+
+    if (link.indexOf('.com') == -1) {
+        window.alert('Esta URL não é válida. Tente novamente.')
+    } else {
+        var lista = ''
+        if (box === 1) {
+            lista = document.getElementById('listsocial')
+        } else if (box === 2) {
+            lista = document.getElementById('listwork')
+        } else {
+            lista = document.getElementById('listenter')
+        }
+        
+        var newlinkdiv = document.createElement('div')
+        newlinkdiv.setAttribute('class', 'cadalink')
+        lista.appendChild(newlinkdiv)
+        var newlink = document.createElement('a')
+        newlink.target = '_blank'
+        newlink.href = `https://www.${link}`
+        newlink.innerHTML = link
+        newlinkdiv.appendChild(newlink)
+        
+        
+
+
+    }
+
+    var remform = document.getElementById('boxform')
+    main.removeChild(remform)
+    
 }
+    
